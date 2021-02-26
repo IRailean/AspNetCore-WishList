17789d24ca67a9f999bf42402e3a1fccc5d3c4bd
Igor Valeriu Railean <igor-valeriu.railean@intern.arobs.com>
2021-02-26 20:28:00 +0200

    Create ItemController.cs

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            try
+            {
+                if (item is null)
+                {
+                    return BadRequest("Item is null");
+                }
+                _context.Add(item);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }      
+        }
+
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var item = _context.Items.FirstOrDefault(x => x.Id == id);
+
+                if (item is null)
+                {
+                    return BadRequest($"Item with id: {id} not found");
+                }
+                _context.Remove(item);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+        }
+    }
+}
